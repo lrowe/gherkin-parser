2731dd6864064cc258908cd4e5c6b3e2f2f2adba
Laurence Rowe <l@lrowe.co.uk>
2015-06-08 16:08:54 -0700

    Python3 compatibility for __main__

--- src/gherkin_parser/__main__.py
+++ src/gherkin_parser/__main__.py
@@ -4,9 +4,11 @@ Main command. Reads a feature file from stdin, outputs it as parsed JSON.
 
 import codecs
 import json
+import six
 import sys
 
 from gherkin_parser import parse_file
 
-with codecs.getreader('utf8')(sys.stdin) as fp:
-    print json.dumps(parse_file(fp), indent=4)
+stdin = sys.stdin if six.PY2 else sys.stdin.buffer
+with codecs.getreader('utf8')(stdin) as fp:
+    print(json.dumps(parse_file(fp), indent=4))
